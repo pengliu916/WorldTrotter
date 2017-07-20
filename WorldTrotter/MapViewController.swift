7576dc6c28137b640eddebd1f913b04723e48902
Peng <pliu3@apple.com>
2017-07-19 18:18:29 -0700

    Setup views using storyboard

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Peng Liu on 7/19/17.
+//  Copyright © 2017 PLStudio. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view")
+    }
+}
