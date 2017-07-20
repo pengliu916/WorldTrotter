f9fc2e2d5c8f19d0c93ca9990877e54ce37e285e
Peng <pliu3@apple.com>
2017-07-20 12:51:19 -0700

    Added localization support

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,10 +7,52 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        // Create a map view
+        mapView = MKMapView()
+        view = mapView
+        
+        let standardStr = NSLocalizedString("Standard", comment: "Standard map view")
+        let satelliteStr = NSLocalizedString("Satellite", comment: "Satellite map view")
+        let hybridStr = NSLocalizedString("Hybrid", comment: "Hybrid map view")
+        
+        let segmentedControl = UISegmentedControl(items: [standardStr, hybridStr, satelliteStr])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        segmentedControl.addTarget(self, action: #selector(MapViewController.mapTypeChanged(_:)), for: .valueChanged)
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("MapViewController loaded its view")
+    }
+    
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
     }
 }
